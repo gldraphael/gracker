45731e97afe0720b60f58ac191d3196adede0a47
Galdin Raphael <gldraphael@users.noreply.github.com>
2022-05-20 07:03:17 +0530

    Configure swagger

--- apps/api/src/Gracker.Api/Program.cs
+++ apps/api/src/Gracker.Api/Program.cs
@@ -1,6 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseSwagger(c => {
+    c.RouteTemplate = "docs/{documentName}/openapi.{json|yaml}";
+});
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("docs/v1/openapi.json", "v1");
+    c.DocumentTitle = "Gracker API";
+    c.RoutePrefix = string.Empty;
+});
 
 app.Run();
