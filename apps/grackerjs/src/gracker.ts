43bce5515d1a4fe70ff0b73b06da48d8e9e5dd3c
Galdin Raphael <gldraphael@users.noreply.github.com>
2022-05-21 14:53:17 +0530

    Don't wait 10s to make the first request

--- apps/grackerjs/src/gracker.ts
+++ apps/grackerjs/src/gracker.ts
@@ -2,29 +2,16 @@ import FingerprintJS from '@fingerprintjs/fingerprintjs'
 
 export async function setupGracker()
 {
-  const URL = 'http://localhost:17702/v1/events'
   const data = {
     fingerprint: await getFingerprint(),
     timezone: Intl.DateTimeFormat().resolvedOptions().timeZone
   }
   const bodyJson = JSON.stringify(data)
 
-  // TODO: explore performing this in a web-worker
-  setInterval(async () => {
-    const response = await fetch(URL, {
-      method: 'POST',
-      cache: 'no-cache',
-      credentials: 'omit',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      redirect: 'follow',
-      referrerPolicy: 'unsafe-url',
-      body: bodyJson
-    });
-    if(response.ok != true) {
-      console.warn(response.statusText)
-    }
+  // send an event immediately, and after every 10 seconds
+  await sendEvent(bodyJson)
+  setInterval(async () => { // TODO: explore ways to do this in a web-worker
+    await sendEvent(bodyJson)
   }, 10_000)
 }
 
@@ -34,4 +21,23 @@ async function getFingerprint() : Promise<string> {
   const result = await fingerprintJs.get()
 
   return result.visitorId
+}
+
+async function sendEvent(bodyJson: string) {
+  const URL = 'http://localhost:17702/v1/events'
+
+  const response = await fetch(URL, {
+    method: 'POST',
+    cache: 'no-cache',
+    credentials: 'omit',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    redirect: 'follow',
+    referrerPolicy: 'unsafe-url',
+    body: bodyJson
+  });
+  if(response.ok != true) {
+    console.warn(response.statusText)
+  }
 }
