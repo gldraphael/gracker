f346b2fc5d29be6bc28b6add6a146ff5d1ce8a00
Galdin Raphael <gldraphael@users.noreply.github.com>
2022-06-04 22:43:18 +0530

    Use the service shell to send incoming events to the worker app

---
+++ libs/Gracker.MessageContracts/EventReceived.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+
+namespace Gracker.MessageContracts;
+
+public record EventReceived(
+    DateTime TimestampUtc, // TODO: reconsider this
+    string Fingerprint,
+    string Timezone,
+    IPAddress? IPAddress
+);
