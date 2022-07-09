abcf8ea7ba2916add120343727f56b54b1c58f3d
Galdin Raphael <gldraphael@users.noreply.github.com>
2022-07-10 00:54:32 +0530

    Add a consumer to save incoming data

---
+++ apps/worker-app/src/Gracker.WorkerApp/Consumers/EventReceivedConsumer.cs
@@ -0,0 +1,28 @@
+﻿using Gracker.MessageContracts;
+using Gracker.WorkerApp.Infrastructure.Data;
+using Gracker.WorkerApp.Infrastructure.Data.Models;
+using MassTransit;
+
+namespace Gracker.WorkerApp.Consumers;
+
+public class EventReceivedConsumer : IConsumer<EventReceived>
+{
+    private readonly GrackerDbContext db;
+
+    public EventReceivedConsumer(GrackerDbContext db)
+    {
+        this.db = db;
+    }
+
+    public async Task Consume(ConsumeContext<EventReceived> context)
+    {
+        var dao = new RawEventDAO
+        {
+            Fingerprint = context.Message.Fingerprint,
+            IPAddress = context.Message.IPAddress,
+            TimestampUtc = context.Message.TimestampUtc
+        };
+        db.RawEvents.Add(dao);
+        await db.SaveChangesAsync().ConfigureAwait(false);
+    }
+}
