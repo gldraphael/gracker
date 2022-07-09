5b46c89f3663b7ab9f6d7d8bb78bbb265267957d
Galdin Raphael <gldraphael@users.noreply.github.com>
2022-07-10 00:44:24 +0530

    Configure a DbContext with EF Core + first migration

---
+++ apps/worker-app/src/Gracker.WorkerApp.Infrastructure.Data/Migrations/20220701214652_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Gracker.WorkerApp.Infrastructure.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RawEvents",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    TimestampUtc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Fingerprint = table.Column<string>(type: "text", nullable: false),
+                    IPAddress = table.Column<IPAddress>(type: "inet", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RawEvents", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RawEvents");
+        }
+    }
+}
---
+++ apps/worker-app/src/Gracker.WorkerApp.Infrastructure.Data/Models/RawEventDAO.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+
+namespace Gracker.WorkerApp.Infrastructure.Data.Models;
+
+public class RawEventDAO
+{
+    public int Id { get; set; }
+    public DateTime TimestampUtc { get; set; }
+    public string Fingerprint { get; set; } = null!;
+    public IPAddress? IPAddress { get; set; }
+}
