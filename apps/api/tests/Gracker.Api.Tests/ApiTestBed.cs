0ea7413d31a4622f4e74b004cddd25b813bde966
Galdin Raphael <gldraphael@users.noreply.github.com>
2022-07-10 21:13:34 +0530

    Setup API tests

---
+++ apps/api/tests/Gracker.Api.Tests/ApiTestBed.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Gracker.Api.Tests;
+
+public class ApiTestBed : IDisposable
+{
+
+    public WebApplicationFactory<Program> Api { get; }
+
+    public ApiTestBed()
+    {
+        Api = new WebApplicationFactory<Program>();
+        Api.WithWebHostBuilder(builder =>
+        {
+            // ... Configure test services
+        });
+    }
+
+
+
+
+
+
+
+    private bool disposedValue;
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposedValue)
+        {
+            if (disposing)
+            {
+                Api.Dispose();
+            }
+            disposedValue = true;
+        }
+    }
+
+    public void Dispose()
+    {
+        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+}
