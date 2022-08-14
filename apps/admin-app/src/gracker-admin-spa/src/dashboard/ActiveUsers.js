0e6f35a45dfcc98ff08ff90746cb0790e9fc4596
Galdin Raphael <gldraphael@users.noreply.github.com>
2022-08-14 21:52:24 +0530

    Refactor things a bit

---
+++ apps/admin-app/src/gracker-admin-spa/src/dashboard/ActiveUsers.js
@@ -0,0 +1,23 @@
+import { Component } from 'react'
+
+export class ActiveUsers extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { activeUsers: 0 };
+  }
+
+  render() {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <p className="text-9xl font-bold">{this.state.activeUsers}</p>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    fetch(`${process.env.REACT_APP_API_BASE}/active-users`)
+      .then(response => response.json())
+      .then(response => this.setState({ activeUsers: response.count }));
+  }
+}
--- apps/admin-app/src/gracker-admin-spa/src/dashboard/Dashboard.js
+++ apps/admin-app/src/gracker-admin-spa/src/dashboard/Dashboard.js
@@ -1,23 +1,8 @@
 import { Component } from 'react'
+import { ActiveUsers } from './ActiveUsers'
 
 export class Dashboard extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = { activeUsers: 0 };
-  }
-
   render() {
-    return (
-      <div className="flex items-center justify-center min-h-screen">
-        <p className="text-9xl font-bold">{this.state.activeUsers}</p>
-      </div>
-    );
-  }
-
-  componentDidMount() {
-    fetch(`${process.env.REACT_APP_API_BASE}/active-users`)
-      .then(response => response.json())
-      .then(response => this.setState({ activeUsers: response.count }));
+    return <ActiveUsers />
   }
 }
